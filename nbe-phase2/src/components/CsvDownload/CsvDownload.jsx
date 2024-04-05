788e7fed9967659a268ff113ac29b12297e139f3
Christy Grothues <122482422+Khryssty@users.noreply.github.com>
2024-04-05 17:08:01 -0400

    Adding in the csv download function.

---
+++ nbe-phase2/src/components/CsvDownload/CsvDownload.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import Papa from 'papaparse';
+
+const DownloadCSVButton = ({ data }) => {
+  const handleDownloadCSV = () => {
+    const csv = Papa.unparse(data);
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'data.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Button colorScheme="blue" variant="solid" size="md" onClick={handleDownloadCSV} margin={3}>
+      Download as CSV
+    </Button>
+  );
+};
+
+export default DownloadCSVButton;
